645f24820547f6ca559dd61b15cd38c10b7e21bc
annalisekimura <kimuraannalise@gmail.com>
2024-08-01 20:29:31 -0400

    projects

--- annalise-website/src/components/Netflix/Components/Education.jsx
+++ annalise-website/src/components/Netflix/Components/Education.jsx
@@ -1,14 +1,89 @@
 import React, {useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+import '../Styles/education.css';
 
 const Education = () => {
+    const courses = [
+        { className: 'CS180', title: 'Problem Solving and Object-Oriented Programming'},
+        { className: 'CS182', title: 'Foundations of Computer Science'},
+        { className: 'CS240', title: 'Programming in C'},
+        { className: 'CS250', title: 'Computer Architecture'},
+        { className: 'CS251', title: 'Data Structures and Algorithms'},
+        { className: 'CS252', title: 'Systems Programming'},
+        { className: 'CS307', title: 'Software Engineering I'},
+        { className: 'CS361', title: 'Great Issues in Computer Science'},
+        { className: 'CS355', title: 'Intro to Cryptography'},
+        { className: 'CS354', title: 'Operating Systems'},
+        { className: 'CS348', title: 'Information Systems'}
+    ]
+
+    const currentCourses = [
+        { className: 'CS381', title: 'Introduction to the Analysis of Algorithms'},
+        { className: 'CS426', title: 'Computer Security'}
+    ]
+
+    const additional = [
+        { className: 'College', title: 'Purdue University- West Lafayette' },
+        { className: 'Major', title: 'BS Computer Science' },
+        { className: 'Track', title: 'Software Engineering' },
+        { className: 'Track', title: 'Computer Security' },
+        { className: 'Year Start', title: '2021' },
+        { className: 'Expected Graduation', title: '2025' },
+        { className: '2023 Summer Intern', title: 'USDA Computer Science Intern'}
+    ]
+
 
     return (
-        <div>
-            <h2>Education</h2>
-        </div>
+        <div className="entire">
+            <div className="browse-courses">
+                <h2>Relevant Courses</h2>
+                <div className="browse-row-course">
+                    {courses.map((course, index) => (
+                        <div key={index} className="placeholder-box-courses">
+                            <p className={`placeholder-image-courses ${course.className}`}>{course.title}</p>
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{course.className}</div>
+                            </div>
+                        </div> 
+                        
+                    ))}
+                </div>
+            </div>
 
+            <div className="browse-current">
+                <h2>Current Courses</h2>
+                <div className="browse-row-current">
+                    {currentCourses.map((current, index) => (
+                        <div key={index} className="placeholder-box-current">
+                            <p className={`placeholder-image-courses ${current.className}`}>{current.title}</p>
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{current.className}</div>
+                            </div>
+                        </div> 
+                        
+                    ))}
+                </div>
+            </div>
+
+            <div className="browse-more">
+                <h2>More Info</h2>
+                <div className="browse-row-more">
+                    {additional.map((more, index) => (
+                        <div key={index} className="placeholder-box-more">
+                            <p className={`placeholder-image-more ${more.className}`}>{more.title}</p>
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{more.className}</div>
+                            </div>
+                        </div> 
+                        
+                    ))}
+                </div>
+            </div>
+
+        </div>
     )
+
+    
 }
 
 
--- annalise-website/src/components/Netflix/Components/Projects.jsx
+++ annalise-website/src/components/Netflix/Components/Projects.jsx
@@ -1,13 +1,207 @@
 import React, {useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+import '../Styles/projects.css';
+
+import myTreeLogin from '../Pictures/myTree/login.png';
+import myTreeQuiz from '../Pictures/myTree/quiz.png';
+import myTreeWelcome from '../Pictures/myTree/welcome.png';
+import myTreeCreateAccount from '../Pictures/myTree/createAccount.png';
+
+import myTreeLeaderboard from '../Pictures/myTree/leaderboard.png';
+import myTreeGroup from '../Pictures/myTree/group.png';
+import myTreeActivities from '../Pictures/myTree/activities.png';
+import myTreeAwards from '../Pictures/myTree/awards.png';
+
+import myTreeNav from '../Pictures/myTree/nav.png';
+import myTreeStats from '../Pictures/myTree/stats.png';
+import myTreeNotifications from '../Pictures/myTree/notifications.png';
+import myTreeManage from '../Pictures/myTree/manage.png';
+
+import beProductiveCongrats from '../Pictures/beProductive/congrats.png';
+import beProductiveDelete from '../Pictures/beProductive/delete.png';
+import beProductiveFinish from '../Pictures/beProductive/finish.png';
+import beProductiveLogin from '../Pictures/beProductive/login.png';
+import beProductiveMonth from '../Pictures/beProductive/month.png';
+import beProductiveNews from '../Pictures/beProductive/news.png';
+import beProductiveNotes from '../Pictures/beProductive/notes.png';
+import beProductiveWeek from '../Pictures/beProductive/week.png';
+
+import spotPadBackground from '../Pictures/SpotPad/background.png';
+import spotPadColors from '../Pictures/SpotPad/colors.png';
+import spotPadCurrent from '../Pictures/SpotPad/current.png';
+import spotPadDraw from '../Pictures/SpotPad/draw.png';
+import spotPadGallery from '../Pictures/SpotPad/gallery.png';
+import spotPadGif from '../Pictures/SpotPad/gif.png';
+import spotPadHome from '../Pictures/SpotPad/home.png';
+import spotPadRightClick from '../Pictures/SpotPad/rightClick.png';
+import spotPadSave from '../Pictures/SpotPad/save.png';
+import spotPadSaved from '../Pictures/SpotPad/saved.png';
+import spotPadSavedEntries from '../Pictures/SpotPad/savedEntries.png';
+import spotPadScrapbook from '../Pictures/SpotPad/scrapbook.png';
+import spotPadTerms from '../Pictures/SpotPad/terms.png';
+import spotPadTracks from '../Pictures/SpotPad/tracks.png';
+import spotPadWelcome from '../Pictures/SpotPad/welcome.png';
+
+import buySellAddBuyer from '../Pictures/buyAndSell/addBuyer.png';
+import buySellAddSeller from '../Pictures/buyAndSell/addSeller.png';
+import buySellBuyListing from '../Pictures/buyAndSell/buyListing.png';
+import buySellEditListing from '../Pictures/buyAndSell/editListing.png';
+import buySellListingForm from '../Pictures/buyAndSell/listingForm.png';
+import buySellListings from '../Pictures/buyAndSell/listings.png';
+import buySellOrders from '../Pictures/buyAndSell/orders.png';
+import buySellSellerPage from '../Pictures/buyAndSell/sellerPage.png';
+import buySellSortListings from '../Pictures/buyAndSell/sortListings.png';
+import buySellSortOrders from '../Pictures/buyAndSell/sortOrders.png';
+import buySellWelcome from '../Pictures/buyAndSell/welcome.png';
 
 const Projects = () => {
+    const myTree = [
+        { src: myTreeCreateAccount, className: 'myTree-create', title: 'Create Account' },
+        { src: myTreeLogin, className: 'myTree-login', title: 'Login' },
+        { src: myTreeQuiz, className: 'myTree-quiz', title: 'Quiz' },
+        { src: myTreeWelcome, className: 'myTree-welcome', title: 'Welcome'},
+        { src: myTreeActivities, className: 'myTree-activities', title: 'Activities'},
+        { src: myTreeAwards, className: 'myTree-awards', title: 'Awards'},
+        { src: myTreeLeaderboard, className: 'myTree-leaderboard', title: 'Leaderboard'},
+        { src: myTreeGroup, className: 'myTree-group', title: 'Group'},
+        { src: myTreeNav, className: 'myTree-nav', title: 'Nav'},
+        { src: myTreeStats, className: 'myTree-stats', title: 'Climate Stats'},
+        { src: myTreeNotifications, className: 'myTree-notifications', title: 'Notifications'},
+        { src: myTreeManage, className: 'myTree-manage', title: 'Manage Account'},
+    ]
+
+    const beProductive = [
+        { src: beProductiveLogin, className: 'beProductive-login', title: 'Login Page' },
+        { src: beProductiveNews, className: 'beProductive-news', title: 'News' },
+        { src: beProductiveMonth, className: 'beProductive-month', title: 'Month' },
+        { src: beProductiveWeek, className: 'beProductive-week', title: 'Week' },
+        { src: beProductiveNotes, className: 'beProductive-notes', title: 'Notes' },
+        { src: beProductiveDelete, className: 'beProductive-delete', title: 'Edit or Delete' },
+        { src: beProductiveFinish, className: 'beProductive-finish', title: 'Finish Task' },
+        { src: beProductiveCongrats, className: 'beProductive-congrats', title: 'Congratulations' }
+    ]
+
+    const spotPad = [
+        { src: spotPadWelcome, className: 'spotPad-welcome', title: 'Welcome' },
+        { src: spotPadTerms, className: 'spotPad-terms', title: 'Spotify Terms' },
+        { src: spotPadHome, className: 'spotPad-home', title: 'Home Page' },
+        { src: spotPadScrapbook, className: 'spotPad-scrapbook', title: 'Scrapbook Area' },
+        { src: spotPadTracks, className: 'spotPad-tracks', title: 'Tracks in Playlists' },
+        { src: spotPadColors, className: 'spotPad-colors', title: 'Color Picker' },
+        { src: spotPadDraw, className: 'spotPad-draw', title: 'Draw' },
+        { src: spotPadRightClick, className: 'spotPad-rightClick', title: 'On Right Click' },
+        { src: spotPadGif, className: 'spotPad-gif', title: 'Search Gifs'},
+        { src: spotPadBackground, className: 'spotPad-background', title: 'Background Color' },
+        { src: spotPadCurrent, className: 'spotPad-current', title: 'Current Drawing' },
+        { src: spotPadSave, className: 'spotPad-save', title: 'Save' },
+        { src: spotPadGallery, className: 'spotPad-gallery', title: 'Gallery' },
+        { src: spotPadSavedEntries, className: 'spotPad-entries', title: 'Saved States'},
+        { src: spotPadSaved, className: 'spotPad-saved', title: 'View Saved Drawing' }
+    ]
+
+    const buySell = [
+        { src: buySellWelcome, className: 'buySell-welcome', title: 'Welcome'},
+        { src: buySellAddSeller, className: 'buySell-addSeller', title: 'Add Seller' },
+        { src: buySellSellerPage, className: 'buySell-sellerPage', title: 'Seller Page' },
+        { src: buySellSortListings, className: 'buySell-sortListings', title: 'Sort Listings'},
+        { src: buySellEditListing, className: 'buySell-editListing', title: 'Edit Listing'},
+        { src: buySellListingForm, className: 'buySell-listingForm', title: 'Listing Form' },
+        { src: buySellListings, className: 'buySell-listings', title: 'Listings' },
+
+        { src: buySellAddBuyer, className: 'buySell-addBuyer', title: 'Add Buyer' },
+        { src: buySellBuyListing, className: 'buySell-buyListing', title: 'Buy Listing' },
+        { src: buySellOrders, className: 'buySell-orders', title: 'Orders' },
+        { src: buySellSortOrders, className: 'buySell-sortOrders', title: 'Sort Orders'}
+    ]
+
 
     return (
-        <div>
-            <h2>Projects</h2>
-        </div>
+        <div className="entire">
+            <div className="browse-myTree">
+                <h2>MyTree</h2>
 
+                <div className="browse-row-myTree">
+                    {myTree.map((tree, index) => (
+                        <div key={index} className="placeholder-box-myTree">
+                            <img
+                                src={tree.src}
+                                alt={`Placeholder ${index + 1}`}
+                                className={`placeholder-image-myTree ${tree.className}`}
+                            />
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{tree.title}</div>
+                            </div>
+                            
+                        </div>
+                        
+                    ))}
+                </div>
+            </div>
+
+            <div className="browse-beProductive">
+                <h2>BeProductive</h2>
+
+                <div className="browse-row-beProductive">
+                    {beProductive.map((productive, index) => (
+                        <div key={index} className="placeholder-box-beProductive">
+                            <img
+                                src={productive.src}
+                                alt={`Placeholder ${index + 1}`}
+                                className={`placeholder-image-beProductive ${productive.className}`}
+                            />
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{productive.title}</div>
+                            </div>
+                            
+                        </div>
+                        
+                    ))}
+                </div>
+            </div>
+
+            <div className="browse-spotPad">
+                <h2>SpotPad</h2>
+
+                <div className="browse-row-spotPad">
+                    {spotPad.map((pad, index) => (
+                        <div key={index} className="placeholder-box-spotPad">
+                            <img
+                                src={pad.src}
+                                alt={`Placeholder ${index + 1}`}
+                                className={`placeholder-image-spotPad ${pad.className}`}
+                            />
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{pad.title}</div>
+                            </div>
+                            
+                        </div>
+                        
+                    ))}
+                </div>
+            </div>
+
+            <div className="browse-buySell">
+                <h2>BuyAndSell</h2>
+
+                <div className="browse-row-buySell">
+                    {buySell.map((item, index) => (
+                        <div key={index} className="placeholder-box-buySell">
+                            <img
+                                src={item.src}
+                                alt={`Placeholder ${index + 1}`}
+                                className={`placeholder-image-buySell ${item.className}`}
+                            />
+                            <div className={`overlay overlay-${index}`}>
+                                <div className="text">{item.title}</div>
+                            </div>
+                            
+                        </div>
+                        
+                    ))}
+                </div>
+            </div>
+
+        </div>
     )
 }
 
